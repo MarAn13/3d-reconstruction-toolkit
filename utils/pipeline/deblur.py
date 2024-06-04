4c0653def5f7096e9189b508f9d9df6259862b66
Anatoly Maremianin <maremianin@gmail.com>
2024-06-04 21:53:09 +0300

    added parallelization for deblurring

--- utils/pipeline/deblur.py
+++ utils/pipeline/deblur.py
@@ -2,7 +2,116 @@ import cv2
 import numpy as np
 from skimage import measure, restoration
 import os
-from tqdm.auto import tqdm
+import tqdm
+from joblib import Parallel, delayed
+
+
+##########################################################################################
+# joblib with tqdm                                                                       #
+# provided by https://github.com/joblib/joblib/issues/972                                #
+##########################################################################################
+class ParallelTqdm(Parallel):
+    """joblib.Parallel, but with a tqdm progressbar
+
+    Additional parameters:
+    ----------------------
+    total_tasks: int, default: None
+        the number of expected jobs. Used in the tqdm progressbar.
+        If None, try to infer from the length of the called iterator, and
+        fallback to use the number of remaining items as soon as we finish
+        dispatching.
+        Note: use a list instead of an iterator if you want the total_tasks
+        to be inferred from its length.
+
+    desc: str, default: None
+        the description used in the tqdm progressbar.
+
+    disable_progressbar: bool, default: False
+        If True, a tqdm progressbar is not used.
+
+    show_joblib_header: bool, default: False
+        If True, show joblib header before the progressbar.
+
+    Removed parameters:
+    -------------------
+    verbose: will be ignored
+
+
+    Usage:
+    ------
+    >>> from joblib import delayed
+    >>> from time import sleep
+    >>> ParallelTqdm(n_jobs=-1)([delayed(sleep)(.1) for _ in range(10)])
+    80%|████████  | 8/10 [00:02<00:00,  3.12tasks/s]
+
+    """
+
+    def __init__(
+        self,
+        *,
+        total_tasks: int | None = None,
+        desc: str | None = None,
+        disable_progressbar: bool = False,
+        show_joblib_header: bool = False,
+        **kwargs,
+    ):
+        if "verbose" in kwargs:
+            raise ValueError(
+                "verbose is not supported. "
+                "Use show_progressbar and show_joblib_header instead."
+            )
+        super().__init__(verbose=(1 if show_joblib_header else 0), **kwargs)
+        self.total_tasks = total_tasks
+        self.desc = desc
+        self.disable_progressbar = disable_progressbar
+        self.progress_bar: tqdm.tqdm | None = None
+
+    def __call__(self, iterable):
+        try:
+            if self.total_tasks is None:
+                # try to infer total_tasks from the length of the called iterator
+                try:
+                    self.total_tasks = len(iterable)
+                except (TypeError, AttributeError):
+                    pass
+            # call parent function
+            return super().__call__(iterable)
+        finally:
+            # close tqdm progress bar
+            if self.progress_bar is not None:
+                self.progress_bar.close()
+
+    __call__.__doc__ = Parallel.__call__.__doc__
+
+    def dispatch_one_batch(self, iterator):
+        # start progress_bar, if not started yet.
+        if self.progress_bar is None:
+            self.progress_bar = tqdm.tqdm(
+                desc=self.desc,
+                total=self.total_tasks,
+                disable=self.disable_progressbar,
+                unit="tasks",
+            )
+        # call parent function
+        return super().dispatch_one_batch(iterator)
+
+    dispatch_one_batch.__doc__ = Parallel.dispatch_one_batch.__doc__
+
+    def print_progress(self):
+        """Display the process of the parallel execution using tqdm"""
+        # if we finish dispatching, find total_tasks from the number of remaining items
+        if self.total_tasks is None and self._original_iterator is None:
+            self.total_tasks = self.n_dispatched_tasks
+            self.progress_bar.total = self.total_tasks
+            self.progress_bar.refresh()
+        # update progressbar
+        self.progress_bar.update(self.n_completed_tasks - self.progress_bar.n)
+
+
+##########################################################################################
+# SECTION                                                                                #
+# END                                                                                    #
+##########################################################################################
 
 
 def variance_of_laplacian(image):
@@ -75,9 +184,24 @@ def deblur_single_driver(image, method="wiener"):
     return deblurred_image
 
 
+def deblur_driver_util(file_path, method):
+    image = cv2.imread(file_path)
+    deblurred_image = deblur_single_driver(image, method=method)
+    cv2.imwrite(file_path, deblurred_image)
+
+
 def deblur_driver(path_to_images, method):
-    for file in tqdm(sorted(os.listdir(path_to_images)), desc="Deblurring.."):
-        file_path = os.path.join(path_to_images, file)
-        image = cv2.imread(file_path)
-        deblurred_image = deblur_single_driver(image, method=method)
-        cv2.imwrite(file_path, deblurred_image)
+    images_abs_paths = [
+        os.path.join(path_to_images, file)
+        for file in sorted(os.listdir(path_to_images))
+    ]
+    ParallelTqdm(
+        n_jobs=-1,
+        total_tasks=len(images_abs_paths),
+        desc="Deblurring..",
+    )(
+        (
+            delayed(deblur_driver_util)(image_abs_path, method)
+            for image_abs_path in images_abs_paths
+        ),
+    )
